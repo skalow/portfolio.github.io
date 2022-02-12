3145f8931adf4c3d3637e62b4b75cd7bfd7488c1
skalowr <skalo.pm@gmail.com>
2022-02-12 05:35:26 -0700

    lateste commet

---
+++ Proyectos/CEM/js/login.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $("#pnlIz").click(function () {
+        if ($(this).hasClass("activo")) {
+            $(this).removeClass("activo");
+            $("#panel").removeClass("activo");
+        }
+        else {
+            $(this).addClass("activo");
+            $("#panel").addClass("activo");
+        }
+    });
+});
